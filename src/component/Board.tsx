5992c218805d31635df15125e3a837ba1e49fe5b
5meang9 <uiop3724@daum.net>
2023-10-08 03:34:22 +0900

    칸반보드 스타일 완성 및 애니메이션추가

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,45 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Badge from 'react-bootstrap/Badge';
 import '../styles/Board.scss';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Board(){
+  useEffect(() => {
+    Aos.init()
+  },[])
+
   return(
     <div className='board-container'>
-      <div className='board-item-wrap'>
+      <div className='board-item-wrap' data-aos="fade-up">
         <div className='board-item-img'>
-          <Badge pill bg="primary">ssss</Badge>
           <img src="images/monsterKill.png" alt="" />
         </div>
+        <Badge pill bg="primary">ssss</Badge>
+        <div className='board-item-contents'>
+          <p className='board-item-period'>개발기간 · 2015.11.01 ~ 2015.11.30</p>
+          <p className='board-item-title'>
+            <img src="images/Python-logo-notext.svg.png"/>
+             GUI 설계기법(게임 만들기)
+          </p>
+        </div>
+      </div>
+      <div className='board-item-wrap' data-aos="fade-up">
+        <div className='board-item-img'>
+          <img src="" alt="" />
+        </div>
+        <Badge pill bg="primary">ssss</Badge>
         <div className='board-item-contents'>
           <p></p>
           <p className='board-item-title'></p>
         </div>
       </div>
-      <div className='board-item-wrap'>
+      <div className='board-item-wrap' data-aos="fade-up">
         <div className='board-item-img'>
-          <Badge pill bg="primary">ssss</Badge>
           <img src="" alt="" />
         </div>
+        <Badge pill bg="primary">ssss</Badge>
         <div className='board-item-contents'>
           <p></p>
           <p className='board-item-title'></p>
         </div>
       </div>
-      <div className='board-item-wrap'>
+      <div className='board-item-wrap' data-aos="fade-up">
         <div className='board-item-img'>
-          <Badge pill bg="primary">ssss</Badge>
           <img src="" alt="" />
         </div>
-        <div className='board-item-contents'>
-          <p></p>
-          <p className='board-item-title'></p>
-        </div>
-      </div>
-      <div className='board-item-wrap'>
-        <div className='board-item-img'>
-          <Badge pill bg="primary">ssss</Badge>
-          <img src="" alt="" />
-        </div>
+        <Badge pill bg="primary">ssss</Badge>
         <div className='board-item-contents'>
           <p></p>
           <p className='board-item-title'></p>
